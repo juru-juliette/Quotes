709ce38f6552eace6948bf8a9fa098af46a22a3c
juru-juliette <kangabejuliette@gmail.com>
2019-08-23 14:46:20 +0200

    creating a class

---
+++ src/app/first.spec.ts
@@ -0,0 +1,7 @@
+import { First } from './first';
+
+describe('First', () => {
+  it('should create an instance', () => {
+    expect(new First()).toBeTruthy();
+  });
+});
---
+++ src/app/first.ts
@@ -0,0 +1,5 @@
+export class First {
+    id:number;
+    name:string;
+    description:string;
+}
