6e6f543c07243d213aa86cc467cafcbcb4888f03
juru-juliette <kangabejuliette@gmail.com>
2019-08-24 14:41:56 +0200

    creating a  form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    GoalFormComponent
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
